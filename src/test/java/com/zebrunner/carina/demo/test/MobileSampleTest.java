4f87a96f6b6752986538328fa50e62b58774a748
azarouski <azarouski@solvd.com>
2023-11-20 10:52:14 +0100

    some stuff for test parallelization was added

--- src/test/java/com/zebrunner/carina/demo/test/MobileSampleTest.java
+++ src/test/java/com/zebrunner/carina/demo/test/MobileSampleTest.java
@@ -33,4 +33,289 @@ public class MobileSampleTest extends TestBase {
         Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
         LOGGER.info("Test finish");
     }
+
+    @Test()
+    public void testLoginUser1() {
+        LOGGER.info("Test start1");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser2() {
+        LOGGER.info("Test start2");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser3() {
+        LOGGER.info("Test start3");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser4() {
+        LOGGER.info("Test start4");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser5() {
+        LOGGER.info("Test start5");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser6() {
+        LOGGER.info("Test start6");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser7() {
+        LOGGER.info("Test start7");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser8() {
+        LOGGER.info("Test start8");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser9() {
+        LOGGER.info("Test start9");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser10() {
+        LOGGER.info("Test start10");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser11() {
+        LOGGER.info("Test start11");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser12() {
+        LOGGER.info("Test start12");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser13() {
+        LOGGER.info("Test start13");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser14() {
+        LOGGER.info("Test start14");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
+
+    @Test()
+    public void testLoginUser15() {
+        LOGGER.info("Test start15");
+        String username = "Test user";
+        String password = RandomStringUtils.randomAlphabetic(10);;
+        WelcomePage welcomePage = new WelcomePage(driver);
+        Assert.assertTrue(welcomePage.isPageOpened(), "Welcome page isn't opened");
+        LoginPage loginPage = welcomePage.clickNextBtn();
+        Assert.assertFalse(loginPage.isLoginBtnActive(), "Login button is active when it should be disabled");
+        LOGGER.info("Test middle");
+        loginPage.typeName(username);
+        loginPage.typePassword(password);
+        loginPage.selectMaleSex();
+        loginPage.checkPrivacyPolicyCheckbox();
+        CarinaDescriptionPage carinaDescriptionPage = loginPage.clickLoginBtn();
+        Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
+        LOGGER.info("Test finish");
+    }
 }
