0a88f8b4ffde3697d2111f1f70b372ff1caaf310
azarouski <azarouski@solvd.com>
2023-11-28 13:11:25 +0100

    some updates

--- src/test/java/com/zebrunner/carina/demo/app/utils/TestBase.java
+++ src/test/java/com/zebrunner/carina/demo/app/utils/TestBase.java
@@ -19,11 +19,11 @@ public class TestBase {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-        LOGGER.info("----------------------");
-        LOGGER.info(System.getenv("DEVICEFARM_LOG_DIR"));
-        LOGGER.info("----------------------");
+//        LOGGER.info("----------------------");
+//        LOGGER.info(System.getenv("DEVICEFARM_LOG_DIR"));
+//        LOGGER.info("----------------------");
         MutableCapabilities capabilities = new MutableCapabilities();
-        capabilities.setCapability("selenium:logLink", System.getenv("DEVICEFARM_LOG_DIR"));
+//        capabilities.setCapability("selenium:logLink", System.getenv("DEVICEFARM_LOG_DIR"));
         driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 //        driver = new AppiumDriver(new URL("https://demo.zebrunner.farm/mcloud/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
